7e368d088f5ec7b393b005141c39038df263c547
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-09-21 18:14:15 -0700

    add submit event listener

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
+// DOM variables
 var todoInput = document.querySelector("#todo-text");
 var todoForm = document.querySelector("#todo-form");
 var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
-var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+// Global variables
+var count = 0;
+var tasks = [];
 
-function renderTodos() {
-  todoList = "";
-  todoCountSpan = 0;
+// when tasks is type, it's added to the lists
 
-  for (var i = 0; i < todos.length; i++) {
-    var liElement = document.createElement("li");
-    liElement.text;
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var todoText = todoInput.value.trim();
+
+  if (todoText === "") {
+    alert("No task was entered");
+  } else {
+    tallyCount();
+    tasks.push(todoText);
+    todoInput.value = "";
+
+    storeTodos();
+    renderTodos();
   }
+});
+
+function tallyCount() {
+  count++;
+  todoCountSpan.innerHTML = count;
 }
-
-renderTodos();
-
-// * Your goal is to create a function that will render our todos into a list in the browser.
-
-//   * Initially set the text content of the todoList to an empty string.
-
-//   * todoCountSpan should show the total count of todos on the page.
-
-// * Inside of your render function you will also need a for loop.
-
-//   * It should loop over the `todos` array creating an `li` element for each index of the array.
-
-//   * It should set the content of the created `li` element to the value of the current array index.
-
-//   * Finally the new `li` should be appended to the `ul` provided.
